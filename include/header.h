be327877d887d24cf176b1c44dc85d75980910ec
I Putu Pawesi Siantika <csiantika@gmail.com>
2023-06-14 00:10:36 +0800

    Added header file

---
+++ include/header.h
@@ -0,0 +1,41 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+/* Librarys*/
+#include <Arduino.h>
+#include "Display.h"
+#include "VoltageSensor.h"
+#include <LiquidCrystal_I2C.h>
+#include "Logger.h"
+#include "CurrentSensor.h"
+
+/* Pins definition*/
+#define PIN_BUTTON 2
+#define PIN_INPUT_VOLTAGE_SENSOR A3
+#define PIN_INPUT_CURRENT_SENSOR A0
+#define PIN_OUTPUT_VOLTAGE_SENSOR A7
+#define PIN_OUTPUT_CURRENT_SENSOR A3
+
+/* Variables*/
+volatile bool button_state = 1;
+typedef struct data_collect
+{
+    float volt;
+    float current;
+} data_collect;
+data_collect data_input;
+data_collect data_output;
+
+/* Global objects*/
+LiquidCrystal_I2C lcd1(0x27, 16, 2);
+LiquidCrystal_I2C lcd2(0x23, 16, 2);
+Display disp1(lcd1); // untuk input
+Display disp2(lcd2); // untuk output
+Logger logger;
+
+// Forward functions declarations
+bool read_button_state(void);
+void no_sd_card_handle(Display &disp1, Display &disp2);
+void log_failed_handle(Display &disp1, Display &disp2);
+
+#endif
